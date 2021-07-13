5442e9ad052197fb9c2a5d5eb960c65c3e4706a6
Drile99 <nikoladrikic5@gmail.com>
2021-07-13 23:22:38 +0200

    Second commit

--- app/src/main/java/com/example/musicplayerproject/Fragments/AllSongFragment.java
+++ app/src/main/java/com/example/musicplayerproject/Fragments/AllSongFragment.java
@@ -27,10 +27,10 @@ import com.example.musicplayerproject.R;
 
 
 import java.util.ArrayList;
-
+//nasledjuje listu fragmenta
 public class AllSongFragment extends ListFragment
 {
-    //Deklarisanje promenljive Contet redolver koja sluzi za prikazivanje kolona direktno u fragmentu
+    //Deklarisanje promenljive Contet redolver  koja sluzi za prikazivanje kolona direktno u fragmentu
     private static ContentResolver contentResolver1;
     //Deklarisanje array liste
     public ArrayList<SongsList> songsList;
